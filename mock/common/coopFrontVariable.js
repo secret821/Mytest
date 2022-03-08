01583cf3517b8c953eccd02964a631e95c997ef2
Master Q <1426286337@qq.com>
2022-03-08 17:46:00 +0800

    mock背景

--- mock/common/coopFrontVariable.js
+++ mock/common/coopFrontVariable.js
@@ -6,6 +6,7 @@ module.exports = {
   "data": {
     "test_config_01": "前端配置项测试",
     "test_config_03": null,
-    "test_config_02": "111"
+    "test_config_02": "111",
+    gameBgm: '//yun.duiba.com.cn/aurora/assets/055bfe07baca654e4d9723283661a61a1d400353.mp3'
   }
 }
