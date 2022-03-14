d4f57eb7e1c0ea31a0371266d9419f0220a1d230
Master Q <1426286337@qq.com>
2022-03-14 11:26:56 +0800

    1

--- sparkrc.js
+++ sparkrc.js
@@ -10,5 +10,5 @@ module.exports ={
   "IMAGE_Q2": 0.8,
   "RES_PATH": "/src/assets/",
   "RES_PATH_PROD": "//yun.duiba.com.cn/spark/v2/temp_base/1646984006037",
-  "JS_PATH_PROD": "https://yun.duiba.com.cn/spark/v2/temp_base/1647226929921/js"
+  "JS_PATH_PROD": "https://yun.duiba.com.cn/spark/v2/temp_base/1647228321426/js"
 }
