c920887c79d43e2b1ac915791950306c35f52bab
“liuyan” <826148658@qq.com>
2022-03-14 10:15:52 +0800

    style:修改标题

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -151,6 +151,24 @@ class Homepage extends React.Component {
     } else {
       onInitShare(false, false)
     }
+
+    $(function () {
+      function pushHistory() {
+        var state = {
+          title: "title",
+          url: "#"
+        };
+        window.history.pushState(state, "title", "#");
+      }
+      pushHistory();
+      window.addEventListener("popstate", function (e) {
+        // alert("我监听到了浏览器的返回按钮事件啦");
+        location.reload();
+        console.log('刷新首页接口')
+        // 根据自己的需求实现自己的功能
+      }, false);
+    });
+
   }
 
   // beginMusic =()=>{
